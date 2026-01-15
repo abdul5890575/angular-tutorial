c8bd72720ffc6ef460030b2af4e8fa0078f1e4f2
Abdul <abdulrehman.shoukat@gmail.com>
2026-01-15 17:04:19 -0500

    conditional rendering

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Roles } from './components/roles/roles';
+import { Master } from './master/master';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Roles],
+  imports: [RouterOutlet,Master],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
